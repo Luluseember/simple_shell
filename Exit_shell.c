d53dad412f9bcae12ab41378f2e1aa4dab4fd9c1
Luluseember <seemberior@gmail.com>
2023-11-18 14:48:33 -0800

    done

--- Exit_shell.c
+++ Exit_shell.c
@@ -28,4 +28,70 @@ int exit(info_t *info)
 	info->err_num = -1;
 	return (-2);
 }
+/**
+ * cd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int cd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			puts(s);
+			putchar('\n');
+			return (1);
+		}
+		puts(_getenv(info, "OLDPWD=")), putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		puts(info->argv[1]), putchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * help - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int help(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	puts("help call works. Function not yet implemented \n");
+	if (0)
+		puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
 
